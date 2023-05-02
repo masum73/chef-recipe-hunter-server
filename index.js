ccb344fcfaaa4c2e2690a5035f990bdad58dc732
Masum Rahman <masumrahman73@gmail.com>
2023-05-03 00:45:28 +0600

    data json added

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const app = express();
 const cors = require('cors');
 const port = 5000;
 
+const chefs = require('./data/chefs.json');
+const recipes = require('./data/recipes.json');
+
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send('Recipes are cooking')
+    console.log(chefs);
+    res.send(chefs)
 });
 
 app.listen(port, () => {
