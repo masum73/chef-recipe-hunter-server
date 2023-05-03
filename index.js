cc36d35a87181cf532e2703b145b75ac89815c04
Masum Rahman <masumrahman73@gmail.com>
2023-05-03 12:14:15 +0600

    code modified

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const cors = require('cors');
 const port = 5000;
 
 const chefs = require('./data/chefs.json');
-const recipes = require('./data/recipes.json');
 
 app.use(cors());
 
@@ -12,6 +11,26 @@ app.get('/', (req, res) => {
     console.log(chefs);
     res.send(chefs)
 });
+
+app.get('/chef/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedChef = chefs.find(c => c.id === parseInt(id))
+    //console.log(selectedChef);
+    res.send(selectedChef)
+});
+
+// app.get('/chef/:chefId/recipe/:recipeId', (req, res) => {
+
+//     const chefId = req.params.chefId;
+//     const recipeId = req.params.recipeId;
+
+//     const selectedChef = chefs.find(c => c.id === parseInt(chefId))
+//     const recipes = selectedChef.recipes || []
+//     const selectedRecipe = recipes.find(sr => sr.id === parseInt(recipeId))
+
+//     console.log(selectedRecipe);
+//     res.send(selectedRecipe)
+// });
 
 
 app.listen(port, () => {
